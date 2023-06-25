243795332ebe3daeacdd6458171581eefc10626b
c00lB055 <81687616+J0CKSTRAP@users.noreply.github.com>
2023-06-25 19:59:27 -0400

    Update Interface.lua

--- Interface.lua
+++ Interface.lua
@@ -38,7 +38,7 @@ else
     end
 end
 
-local Rain = game:GetObjects("rbxassetid://13580655431")[1]
+local Rain = game:GetObjects("rbxassetid://13793341235")[1]
 Rain.Name = interface_name
 
 function Interface:BeginMenu(menu_options)
